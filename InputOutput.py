309ba71ef66fb4d598264024b5508d2f6c443102
Shaun Thomas <54674474+thoma854@users.noreply.github.com>
2021-10-03 18:32:38 -0400

    creating basic structure for I/O Class.
    started putting methods and planning out how this class/file will look like, and how other classes work off of it.

--- InputOutput.py
+++ InputOutput.py
@@ -1,1 +1,12 @@
-
+class IO:
+  def __init__(self):
+    #default constructor
+    #set up system for communicating with the user
+  
+  def takeInput(): 
+    #accept input from user. 
+    
+  def startChatModel():
+    #start up the chat model once the IO constructor is called. 
+    #call constructor for ChatModel class.     
+ 
