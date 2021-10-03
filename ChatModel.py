7f3c1beb663643124a13c52640031aa7d9ee6c9a
Shaun Thomas <54674474+thoma854@users.noreply.github.com>
2021-10-03 18:58:05 -0400

    create very basic ChatModel class
    created the ChatModel class. This is the class that will utilize natural language processing, and where will I have to use chatbot libraries to write code. 
    For now I have started structuring the class by writing out some methods that will be utilized here. This is where all our sequences of action will be rolled out.

---
+++ ChatModel.py
@@ -0,0 +1,23 @@
+class ChatModel: 
+  def __init__(self):
+    #constructor for the ChatModel class
+    #do necessary setup with Chatbot libraries
+    
+  def parse_user_response(command):
+    #method for parsing the response from the user. 
+    #create Task. 
+    #call perform_task() function. 
+    
+  def determine_task:
+    #method called in parse_user_response to read the user response and set the type of task - Automation, Notification, or Request.
+    #Returns task type. 
+    
+  def perform_task(Task):
+    #method that is called to actually perform the operations necessary to fulfill user's request
+    #give success or fail result, and accordingly call send_response. 
+    
+  def send_response:
+    #method for sending back a response to the user. 
+    #This is called after the actions necessary to fulfill the request has been performed.
+    #necessary information is passed in as parameter. 
+    
