060f195300a80ab9835fe5d8ef21c37a388623ab
KX <kxiong2001@hotmail.com>
2021-10-09 04:54:44 -0400

    Separated authentication and api calls. Added a class for api calls

--- Authentication.py
+++ Authentication.py
@@ -2,9 +2,9 @@ import requests
 from lxml import etree
 
 HEADER = {
-    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.121 Safari/537.36",
-    "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9"
-}
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.121 Safari/537.36",
+            "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9"
+        }
 
 # Returns a requests.session that is logged in to brightspace
 # with the given username and password.
@@ -19,8 +19,8 @@ HEADER = {
 
 def get_brightspace_session(username, password):
     session = requests.session()
-    session = login_purdue_cas(session, username=username, password=password)
-    session = login_brightspace(session)
+    session = __login_purdue_cas(session, username=username, password=password)
+    session = __login_brightspace(session)
     return session
     
 
@@ -33,7 +33,7 @@ def get_brightspace_session(username, password):
 #
 # return: requests.session
 
-def login_purdue_cas(session, username, password):
+def __login_purdue_cas(session, username, password):
 
     res = session.get("https://www.purdue.edu/apps/account/cas/login", headers=HEADER)
 
@@ -65,7 +65,7 @@ def login_purdue_cas(session, username, password):
 # session (requests.session) - a session logged in to Purdue cas.
 # return: requests.session
 
-def login_brightspace(session):
+def __login_brightspace(session):
     res = session.get("https://purdue.brightspace.com/d2l/lp/auth/saml/initiate-login?entityId=https://idp.purdue.edu/idp/shibboleth")
     res.raise_for_status()
 
@@ -78,32 +78,3 @@ def login_brightspace(session):
 
     return session
 
-
-# Testing function to test if we actually logged in to BS!
-# Prints names and end dates of all quizzes posted for given course
-#
-# session (requests.session): A session logged in to BS
-# course_id (str): the id of the course
-# return: None
-
-def get_quizzes(session, course_id):
-    quiz_url = "https://purdue.brightspace.com/d2l/api/le/1.38/{course_id}/quizzes/".format(course_id=course_id)
-    print("q url:", quiz_url)
-    res = session.get(quiz_url, headers=HEADER)
-    
-    quizzes = res.json()["Objects"]
-    for quiz in quizzes:
-        print(quiz["Name"], "  ", quiz["EndDate"])
-
-
-
-def main():
-    session = get_brightspace_session("user", "xxx,push")
-    quiz = get_quizzes(session, "xxx")
-
-
-if __name__ == "__main__":
-    #res = requests.get("https://purdue.brightspace.com/d2l/api/le/1.38/{course_id}/quizzes/".format(course_id=335578))
-    #print(res.status_code)
-    print("---------------------------------")
-    main()
