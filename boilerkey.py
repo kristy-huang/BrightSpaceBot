c98dc0ea1974e9be337f5b876bc9f60b392047e5
raveenamnair <nair64@purdue.edu>
2021-10-10 01:23:17 -0400

    Fixed multiple modules in TOC problem

--- boilerkey.py
+++ boilerkey.py
@@ -1,5 +1,8 @@
 import requests
 from lxml import etree
+import os
+from pathlib import Path
+from File import File, StorageTypes
 
 HEADER = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.121 Safari/537.36",
@@ -8,6 +11,8 @@ HEADER = {
 
 BP_VERSION = "1.38"
 WHO_AM_I_VERSION = "1.0"
+FILE_ARRAY = []
+
 
 # Returns a requests.session that is logged in to brightspace
 # with the given username and password.
@@ -91,7 +96,8 @@ def login_brightspace(session):
 # return: None
 
 def get_quizzes(session, course_id):
-    quiz_url = "https://purdue.brightspace.com/d2l/api/le/{version}/{course_id}/quizzes/".format(version=BP_VERSION, course_id=course_id)
+    quiz_url = "https://purdue.brightspace.com/d2l/api/le/{version}/{course_id}/quizzes/".format(version=BP_VERSION,
+                                                                                                 course_id=course_id)
     print("q url:", quiz_url)
     res = session.get(quiz_url, headers=HEADER)
 
@@ -124,31 +130,51 @@ def get_grade(session, course_id):
     return fraction_string, percentage_string
 
 
-def download__file(session):
-    url = "https://purdue.brightspace.com/d2l/api/le/1.38/335757/content/toc"
+def download__file(session, courseid):
+    url = "https://purdue.brightspace.com/d2l/api/le/1.38/{course_id}/content/toc".format(course_id=courseid)
     print("q url:", url)
     res = session.get(url, headers=HEADER, allow_redirects=True)
 
     print(res.status_code)
-    topic = res.json()["Modules"][0]["Modules"]
-    #[1]["Topics"]
-    s = len(topic)
-    for i in range(s):
-        m = topic[i]["Topics"]
-        for j in range(len(m)):
-            print(m[j]["Url"])
-    #open('facebook.pdf', 'wb').write(res.content)
+    modules = res.json()["Modules"]
+    # number of big sections
+    print(len(modules))
+    # going through the big sections
+    for i in range(len(modules)):
+        print(modules[i]["Title"])
+        # go through any folders the module section may have (module inside module)
+        for j in range(len(modules[i]["Modules"])):
+            print(modules[i]["Modules"][j]["Title"])
+            m_topics = modules[i]["Modules"][j]["Topics"]
+            # going through the topics to see files listed
+            for k in range(len(m_topics)):
+                # print(m_topics[k]["Url"])
+                suffix = Path(m_topics[k]["Url"]).suffixes
+                extension = suffix[len(suffix) - 1]
+                print(extension)
+                if extension == ".pdf":
+                    # todo make another method that processes requests based on topic id
+                    # todo add a field 'topicId' inside of File object
+                    file = File(m_topics[k]["Title"], m_topics[k]["Url"])
+                    FILE_ARRAY.append(file)
+                    # open(m_topics[k]["Url"], 'wb').write(res.content)
+                    # source = m_topics[k]["Url"]
+                    # destination = "/Users/raveena/Desktop/testing" + "/" + source
+                    # os.rename(source, destination)
+
+    return FILE_ARRAY
 
 
 def main():
     session = get_brightspace_session("xxxx", "xxxx,push")
-    #quiz = get_quizzes(session, "335578")
-    #get_grade(session, "335578")
-    download__file(session)
+    quiz = get_quizzes(session, "xxxx")
+    get_grade(session, "xxxx")
+    print("--------------------------")
+    files = download__file(session, "335757")
+    for i in files:
+        print(i.get_info())
 
 
 if __name__ == "__main__":
-    # res = requests.get("https://purdue.brightspace.com/d2l/api/le/1.38/{course_id}/quizzes/".format(course_id=335578))
-    # print(res.status_code)
     print("---------------------------------")
     main()
