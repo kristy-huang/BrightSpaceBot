e47f9e8b8b3430cb5523c1c263b12aa756d88485
raveenamnair <nair64@purdue.edu>
2021-10-23 15:45:29 -0400

    wrote code to insert event to calendar

---
+++ Google.py
@@ -0,0 +1,60 @@
+import pickle
+import os
+import datetime
+from google_auth_oauthlib.flow import Flow, InstalledAppFlow
+from googleapiclient.discovery import build
+from googleapiclient.http import MediaFileUpload, MediaIoBaseDownload
+from google.auth.transport.requests import Request
+
+
+def Create_Service(client_secret_file, api_name, api_version, *scopes, prefix=''):
+    CLIENT_SECRET_FILE = client_secret_file
+    API_SERVICE_NAME = api_name
+    API_VERSION = api_version
+    SCOPES = [scope for scope in scopes[0]]
+
+    cred = None
+    working_dir = os.getcwd()
+    token_dir = 'token files'
+    pickle_file = f'token_{API_SERVICE_NAME}_{API_VERSION}{prefix}.pickle'
+
+    ### Check if token dir exists first, if not, create the folder
+    if not os.path.exists(os.path.join(working_dir, token_dir)):
+        os.mkdir(os.path.join(working_dir, token_dir))
+
+    if os.path.exists(os.path.join(working_dir, token_dir, pickle_file)):
+        with open(os.path.join(working_dir, token_dir, pickle_file), 'rb') as token:
+            cred = pickle.load(token)
+
+    if not cred or not cred.valid:
+        if cred and cred.expired and cred.refresh_token:
+            cred.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(CLIENT_SECRET_FILE, SCOPES)
+            cred = flow.run_local_server()
+
+        with open(os.path.join(working_dir, token_dir, pickle_file), 'wb') as token:
+            pickle.dump(cred, token)
+
+    try:
+        service = build(API_SERVICE_NAME, API_VERSION, credentials=cred)
+        print(API_SERVICE_NAME, API_VERSION, 'service created successfully')
+        return service
+    except Exception as e:
+        print(e)
+        print(f'Failed to create service instance for {API_SERVICE_NAME}')
+        os.remove(os.path.join(working_dir, token_dir, pickle_file))
+        return None
+
+
+def convert_to_RFC_datetime(year=1900, month=1, day=1, hour=0, minute=0):
+    dt = datetime.datetime(year, month, day, hour, minute, 0).isoformat() + 'Z'
+    return dt
+
+
+if __name__ == '__main__':
+    API_NAME = 'calendar'
+    API_VERSION = 'v3'
+    SCOPES = ['https://www.googleapis.com/auth/calendar']
+    CLIENT_FILE = 'client-secret.json'
+    service = Create_Service(CLIENT_FILE, API_NAME, API_VERSION, SCOPES, 'x')
