8c51470ffe99dc99db5976116e6ffae516a4f300
Kristy <kristy.huang15@gmail.com>
2021-12-06 16:34:36 -0500

    template for page

---
+++ web/src/components/Template.js
@@ -0,0 +1,16 @@
+import React, {useState} from 'react'
+import {Form, Button} from 'react-bootstrap'
+import {Link} from 'react-router-dom'
+import {useForm} from 'react-hook-form'
+import {useAuth, logout} from '../auth'
+
+
+function Template(props) {
+    return (
+        <div className="container">
+            // html code
+        </div>
+    )
+}
+
+export default Template
